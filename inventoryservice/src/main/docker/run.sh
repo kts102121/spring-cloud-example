108fcc28c54c686418ee6c992590c7e29181a0c0
kts102121 <ron@vrexlab.com>
2019-01-30 15:33:57 +0800

    fix) introduced inventory service
    Made a skeleton for inventory service. This will be called from
    userservice using service discovery and gateway using ribbon

---
+++ inventoryservice/src/main/docker/run.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+getPort() {
+    echo $1 | cut -d : -f 3 | xargs basename
+}
+
+echo "********************************************************"
+echo "Waiting for the consul server to start on port $CONSUL_PORT"
+echo "********************************************************"
+while ! `nc -z consulserver $CONSUL_PORT`; do sleep 3; done
+echo "******* Consul Server has started"
+
+echo "********************************************************"
+echo "Waiting for the configuration server to start on port $CONFIG_PORT"
+echo "********************************************************"
+while ! `nc -z configserver $CONFIG_PORT`; do sleep 3; done
+echo "*******  Configuration Server has started"
+
+echo "********************************************************"
+echo "Starting Inventory Service  "
+echo "********************************************************"
+java -cp app:app/lib/* \
+     -Djava.security.egd=file:/dev/./urandom \
+     -Dserver.port=$SERVER_PORT \
+     -Dspring.cloud.config.uri=$CONFIG_URL \
+     -Dspring.profiles.active=$PROFILE \
+     org.ron.inventoryservice.InventoryServiceApplication
