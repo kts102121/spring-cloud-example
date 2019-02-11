5265cc7690b63bb4887b3462996ef26c290587a1
kts102121 <ron@vrexlab.com>
2019-02-11 17:01:47 +0900

    add) auth service skeleton
    Project outline for auth service. This will be used to issue jwt tokens

---
+++ authservice/src/main/java/org/ron/authservice/AuthServiceApplication.java
@@ -0,0 +1,15 @@
+package org.ron.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@SpringBootApplication
+@EnableResourceServer
+@EnableAuthorizationServer
+public class AuthServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+}
